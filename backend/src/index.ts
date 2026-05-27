7528ceaf25a99daf667433494123c62ce9582b6f
radben97 <radben.97@gmail.com>
2026-05-27 17:35:04 +0530

    added hotel feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,7 +34,7 @@ app.use("/api/auth",authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/my-hotels",hotelRoutes)
 
-app.get("(.*)", (req: Request, res: Response) => {
+app.get((req: Request, res: Response) => {
     res.sendFile(path.join(import.meta.dirname, "../../frontend/dist/index.html"))
 })
 
